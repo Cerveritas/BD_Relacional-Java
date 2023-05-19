35938406400aeab7fedaafec915d395b95d17db2
SergioCerveraJ <seceji@gmail.com>
2023-05-19 14:13:46 +0200

    19/05/23

---
+++ src/ExamenOrdinaria_1erTrimestre/Asignatura.java
@@ -0,0 +1,59 @@
+package ExamenOrdinaria_1erTrimestre;
+
+public class Asignatura {
+
+    private int id;
+    private String nombre;
+    private double nota_examen;
+    private double nota_practica;
+
+    public Asignatura(int id, String nombre, double nota_examen, double nota_practica) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nota_examen = nota_examen;
+        this.nota_practica = nota_practica;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota_examen() {
+        return nota_examen;
+    }
+
+
+    public void setNota_examen(double nota_examen) {
+        this.nota_examen = nota_examen;
+    }
+
+    public double getNota_practica() {
+        return nota_practica;
+    }
+
+    public void setNota_practica(double nota_practica) {
+        this.nota_practica = nota_practica;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", nota_examen=" + nota_examen +
+                ", nota_practica=" + nota_practica +
+                '}';
+    }
+}
--- src/ExamenOrdinaria_1erTrimestre/main2.java
+++ src/ExamenOrdinaria_1erTrimestre/main2.java
@@ -1,12 +1,23 @@
 package ExamenOrdinaria_1erTrimestre;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
 import java.util.Scanner;
 
 public class main2 {
 
+
+    static double totalProgramacion;
+    static double totalBaseDatos;
+    static double totalEntornos;
+
+    static Connection conn;
     static Scanner sc = new Scanner(System.in);
     static double [][] notas = null;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
 
         int opcion = 0;
@@ -22,6 +33,7 @@ public class main2 {
                     "2. - Base de datos" +
                     "3. - Entornos de Desarrollo");
             System.out.println("4 - Nota media de todas las asignaturas");
+            System.out.println("5 - Crear BBDD notas");
             System.out.println(" ");
             System.out.println("Introduzca una opcion");
             System.out.println("----------------------------------------------");
@@ -39,13 +51,23 @@ public class main2 {
                     break;
 
                 case 2:
+                    rellenarArrayBidimensional();
                     break;
 
                 case 3:
+                    int i = 0;
+                    System.out.println("Elige la asignatura que desea ver la nota media");
+                    i = sc.nextInt();
+                    notaMediaPorAsignatura(i);
                     break;
 
                 case 4:
+                    double medial = notaMediaTodas();
+                    System.out.println("La nota media de todas las asignaturas es: " + medial);
                     break;
+
+                case 5:
+                    setup_bd();
 
                 default:
                     System.out.println("Introduzca una opcion valida");
@@ -56,10 +78,87 @@ public class main2 {
 
     private static void crearArrayBidimensional() {
         notas = new double[3][2];
+    }
+
+    private static void rellenarArrayBidimensional() {
+        System.out.println("Introduzca la nota del examen de de programacion");
+            double notaExamenProgramacion = sc.nextDouble();
+        System.out.println("introduzca la nota de la practica de programacion");
+            double notaPracticaProgramacion = sc.nextDouble();
 
 
+        System.out.println("introduzca la nota del examen de base de datos");
+            double notaExamenBaseDatos = sc.nextDouble();
+        System.out.println("introduzca la nota de la practica de base de datos");
+            double notaPracticaBaseDatos = sc.nextDouble();
+
+
+        System.out.println("introduzca la nota del examen de entornos de desarrollo");
+            double notaExamenEntornosDesarrollo = sc.nextDouble();
+        System.out.println("introduzca la nota de la practica de entornos de desarrollo");
+            double notaPracticaEntornosDesarrollo = sc.nextDouble();
+
+
+            notas[0][0] = notaExamenProgramacion;
+            notas[0][1] = notaPracticaProgramacion;
+
+
+            notas[1][0] = notaExamenBaseDatos;
+            notas[1][1] = notaPracticaBaseDatos;
+
+
+            notas[2][0] = notaExamenEntornosDesarrollo;
+            notas[2][1] = notaPracticaEntornosDesarrollo;
+
+            /*for (int i = 0; i < notas.length; i++) {
+                for (int j = 0; j < notas[i].length; j++) {
+                    //switch case;
+                }
+            }
+*/
+    }
+
+    public static void notaMediaPorAsignatura(int i) {
+
+        switch (i) {
+            case 1:
+                System.out.println("La nota media de Programacion es: " + ((((notas[0][0]/100)*60)  +   (notas[0][1]/100)*40)));
+                //totalProgramacion = sc.nextDouble();
+                break;
+            case 2:
+                System.out.println("La nota media de BBDD es: " + ((((notas[1][0]/100)*60)  +   (notas[1][1]/100)*40)));
+                //totalBaseDatos = sc.nextDouble();
+                break;
+            case 3:
+                System.out.println("La nota media de Entornos de Desarrollo es: "+ ((((notas[2][0]/100)*60)  +   (notas[2][1]/100)*40)));
+                //totalEntornos = sc.nextDouble();
+                break;
+
+            default:
+                System.out.println("Introduzca una opcion valida");
+        }
     }
 
 
+    private static void establecerConexion() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "admin");
+    }
+
+    public static double notaMediaTodas() {
+        double media = 0;
+
+        media = ( ((((notas[0][0]/100)*60)  +   (notas[0][1]/100)*40)) + ((((notas[1][0]/100)*60)  +   (notas[1][1]/100)*40)) + ((((notas[2][0]/100)*60)  +   (notas[2][1]/100)*40)) ) / 3;
+
+       return media;
+    }
+
+    public static void setup_bd() throws SQLException {
+        establecerConexion();
+
+        String query = "CREATE DATABASE Notas ";
+
+        Statement st = conn.createStatement();
+        st.executeUpdate(query);
+    }
 }
 
